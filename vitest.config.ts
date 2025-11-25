1cefe347adad487a84b2e89cf9538d35c546700c
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-11-25 18:12:45 +0700

    chore: update config storybook

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
 /// <reference types="vitest" />
 import { getViteConfig } from 'astro/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
 
 export default getViteConfig({
+  plugins: [react(), tsconfigPaths()],
+
   // @ts-expect-error: 'test' is a Vitest config, not a Vite config
   test: {
     globals: true,
