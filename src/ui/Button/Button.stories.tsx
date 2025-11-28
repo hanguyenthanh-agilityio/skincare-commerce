24a627374b92efc68ff9d73b85ce91a195167f62
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-11-28 10:53:49 +0700

    feat: move icon into component folder

--- src/ui/Button/Button.stories.tsx
+++ src/ui/Button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Button } from '.';
-import { Icons } from '@/icons';
+import { Icons } from '@/components';
 
 const meta: Meta<typeof Button> = {
   title: 'UI/Button',
