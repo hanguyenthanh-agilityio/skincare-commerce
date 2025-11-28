0cc4ba988dc0a67f1d470f57f96156248648ebdd
Ngan.Do <ngan.do@asnet.com.vn>
2025-11-28 13:45:36 +0700

    feat: implement Heading components and NormalBlock; update Welcome component and tests

---
+++ src/components/Blocks/index.ts
@@ -0,0 +1,6 @@
+export { default as Heading2 } from './Heading2/index.astro';
+export { default as Heading3 } from './Heading3/index.astro';
+export { default as Heading4 } from './Heading4/index.astro';
+export { default as Heading5 } from './Heading5/index.astro';
+export { default as Heading6 } from './Heading6/index.astro';
+export { default as NormalBlock } from './NormalBlock/index.astro';
