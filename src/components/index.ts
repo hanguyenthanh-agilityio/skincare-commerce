5d3b64f6cf02c17f020d72db04307ec67ab42291
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-12-02 09:08:57 +0700

    feat: implement Link Wrapper with tsx file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Link } from './Link/index.astro';
 export { default as Navbar } from './Navbar/index.astro';
 export { default as MegaMenu } from './MegaMenu';
+export { default as LinkWrapper } from './LinkWrapper';
