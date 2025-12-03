025c9e79bfb71b69b521e180423c4989f4d624a6
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-12-03 09:07:10 +0700

    feat: update naming

--- src/components/DropdownMenu/index.tsx
+++ src/components/DropdownMenu/index.tsx
@@ -20,7 +20,7 @@ const DropdownMenu = memo(({ data }: DropdownMenuProps) => {
             <Button
               variant="ghost"
               type="button"
-              role="DrawerMenuItem"
+              role="menuitem"
               className="px-0"
               aria-haspopup="true"
               aria-expanded="false"
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -3,12 +3,12 @@ export interface MenuColumn {
   items: string[];
 }
 
-export interface DrawerMenuItem {
+export interface MenuItem {
   title: string;
   columns: MenuColumn[];
   imageUrl: string;
 }
 
 export interface DropdownMenuProps {
-  data: DrawerMenuItem[];
+  data: MenuItem[];
 }
