99e54403027ed5563e4328535f48121d2f9dcaa6
Ngan.Do <ngan.do@asnet.com.vn>
2025-12-05 16:23:04 +0700

    feat: create footer data in constants folder

---
+++ src/constants/footer.ts
@@ -0,0 +1,48 @@
+import twitter from '@/assets/images/twitter.svg';
+import instagram from '@/assets/images/instagram.svg';
+import facebook from '@/assets/images/facebook.svg';
+
+export const SOCIAL_LINKS = [
+  { icon: twitter, label: 'Twitter', href: 'https://x.com' },
+  { icon: instagram, label: 'Instagram', href: 'https://www.instagram.com' },
+  { icon: facebook, label: 'Facebook', href: 'https://www.facebook.com' },
+];
+
+export const NAVIGATION = [
+  {
+    title: 'Products',
+    links: [
+      { label: 'Inner Care', href: '#' },
+      { label: 'Skin Care', href: '#' },
+      { label: 'Scalp Care', href: '#' },
+    ],
+  },
+  {
+    title: 'Guides',
+    links: [
+      { label: 'News', href: '#' },
+      { label: 'Vision', href: '#' },
+      { label: 'Q&A', href: '#' },
+    ],
+  },
+  {
+    title: 'Service',
+    links: [
+      { label: 'About Concierge', href: '#' },
+      { label: 'Online Consultation', href: '#' },
+      { label: 'Market', href: '#' },
+    ],
+  },
+  {
+    title: 'Contact',
+    links: [{ label: 'Contact Us', href: '#' }],
+  },
+];
+
+export const FOOTER_LINKS = [
+  { label: 'Company Profile', href: '#' },
+  { label: 'Privacy policy', href: '#' },
+  { label: 'Cancellation policy', href: '#' },
+  { label: 'Terms of service', href: '#' },
+  { label: 'Refund/Return Policy', href: '#' },
+];
