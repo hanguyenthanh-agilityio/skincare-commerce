85b39f1ee7d5bb799e654af7fc9bec76b0280d9a
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-12-08 16:08:39 +0700

    feat: remove unused code

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,2 @@
 export * from './navigation';
 export * from './userVoiceImages';
-export * from './hero';
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,2 +1,3 @@
+export * from './helper';
 export * from './messages';
 export * from './loader';
