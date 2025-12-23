e4b6623d9ce428fd9dfbe141f0ccacfe3275e5ae
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-12-23 14:51:12 +0700

    feat: update mock data

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -87,3 +87,43 @@ export const PRODUCT_INFO = {
     },
   ],
 };
+
+export const BLOG_DETAIL_CONTENT = {
+  hero: {
+    title:
+      'Countless solutions have been discovered in nature. We simply need to observe and tap into its inherent brilliance',
+    description:
+      'Countless solutions have been discovered in nature. We simply need to observe and tap into its inherent brilliance',
+  },
+
+  section: {
+    title: 'Harnessing nature’s intelligence to create advanced skincare solutions',
+    description:
+      'By studying botanical actives and skin biology, we transform natural discoveries into clinically proven formulations.',
+  },
+};
+
+export const BLOG_HERO_IMAGE = {
+  id: 10,
+  url: 'https://www.gloskinbeauty.com/cdn/shop/articles/668448456942_0f2549ad-4f14-4e19-81c6-23fa6fca465d.jpg?v=1760637019',
+  alternativeText: 'Blog hero image',
+  width: 1600,
+  height: 900,
+};
+
+export const BLOG_IMAGE = {
+  main: {
+    id: 1,
+    url: 'https://canada.gloskinbeauty.com/cdn/shop/files/egf-facial-23-stylized_3_f38ab37e-fc2a-4ed4-80bb-572e59481da7.jpg?v=1756303945',
+    alternativeText: 'Hero product image',
+    width: 1600,
+    height: 1000,
+  },
+  side: {
+    id: 2,
+    url: 'https://www.gloskinbeauty.com/cdn/shop/articles/668462285038_cd82d0bb-d3fd-4003-9808-ac35e1b04146.jpg?v=1760636983',
+    alternativeText: 'Product detail texture',
+    width: 800,
+    height: 600,
+  },
+};
