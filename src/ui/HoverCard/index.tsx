4e71b5925019c580be5f54ef57a4a322822747f1
ngan.do <ngan.do@asnet.com.vn>
2025-12-29 17:50:18 +0700

    fix: cn import consistency

--- src/ui/HoverCard/index.tsx
+++ src/ui/HoverCard/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as HoverCardPrimitive from '@radix-ui/react-hover-card';
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib';
 
 function HoverCard(props: React.ComponentProps<typeof HoverCardPrimitive.Root>) {
   return <HoverCardPrimitive.Root {...props} />;
