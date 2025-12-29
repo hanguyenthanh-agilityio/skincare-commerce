c09e4dc43fd9356943b874ccdc122c9622393826
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-12-29 17:20:21 +0700

    feat: update type

--- src/components/index.ts
+++ src/components/index.ts
@@ -19,7 +19,7 @@ export { default as FaqAccordion } from './FaqAccordion';
 export { default as RadioDropdown } from './RadioDropdown';
 export { default as CategoryFilter } from './CategoryFilter';
 export { default as SkinTypeFilter } from './SkinTypeFilter';
-export { default as QuantitySelect } from './Cart/QuantitySelect';
+export { default as QuantitySelect } from './QuantitySelect';
 
 // Export with astro file
 export { default as Link } from './Link/index.astro';
